67cc1709f2f5ebb436e50af4f4da18718ae64b4a
marco <marcodecrema@libero.it>
2020-11-04 15:20:47 +0100

    seeder

---
+++ database/seeds/ComicsTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Comic;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i<15; $i++) {
+
+            $newComic=new Comic;
+            
+            $newComic->title=$faker->word;        
+            $newComic->original_title=$faker->word;
+            $newComic->author=$faker->name;
+            $newComic->image=$faker->imageUrl;
+            $newComic->year=$faker->year;
+            $newComic->amount=$faker->randomFloat;
+
+            $newComic->save();
+        }
+    }
+}
